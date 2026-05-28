02dd3ddc706fa696f8e3193369ed9a1df632aec7
Zetix3 <zetix1994@gmail.com>
2026-05-28 23:23:38 +0300

    lab 3 doing

---
+++ ZinovevLab33/ZinovevLab33/Form1.cs
@@ -0,0 +1,44 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZinovevLab33
+{
+    public partial class Form1 : Form
+    {
+        View ViewObject = new View();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            ViewObject.InitShaders();
+        }
+
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            ViewObject.Draw();
+            glControl1.SwapBuffers();
+        }
+
+    }
+}
+
---
+++ ZinovevLab33/ZinovevLab33/RayTracing.cs
@@ -0,0 +1,80 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZinovevLab33
+{
+    class View
+    {
+        int BasicProgramID;
+        int BasicVertexShader;
+        int BasicFragmentShader;
+        int vbo_position;
+        int attribute_vpos;
+
+        void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        public void InitShaders()
+        {
+            BasicProgramID = GL.CreateProgram();
+            loadShader("..\\..\\raytracing.vert", ShaderType.VertexShader, BasicProgramID,
+            out BasicVertexShader);
+            loadShader("..\\..\\raytracing.frag", ShaderType.FragmentShader, BasicProgramID,
+            out BasicFragmentShader);
+            GL.LinkProgram(BasicProgramID);
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+            attribute_vpos = GL.GetAttribLocation(BasicProgramID, "vPosition");
+            //int uniform_pos = GL.GetUniformLocation(BasicProgramID, "uCameraPos");
+            //int uniform_aspect = GL.GetUniformLocation(BasicProgramID, "uAspect");
+
+            Vector3[] vertdata = new Vector3[] {
+            new Vector3(-1f, -1f, 0f),
+            new Vector3( 1f, -1f, 0f),
+            new Vector3( 1f,  1f, 0f),
+            new Vector3(-1f,  1f, 0f) };
+
+            GL.GenBuffers(1, out vbo_position);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo_position);
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length *
+            Vector3.SizeInBytes), vertdata, BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(attribute_vpos);
+            GL.VertexAttribPointer(attribute_vpos, 3, VertexAttribPointerType.Float, false, 0, 0);
+            //GL.Uniform3(uniform_pos, campos);
+            //GL.Uniform1(uniform_aspect, aspect);
+        }
+
+        public void Draw()
+        {
+            GL.UseProgram(BasicProgramID);
+
+            GL.DrawArrays(PrimitiveType.TriangleFan, 0, 4);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+
+
+    }
+    internal class RayTracing
+    {
+    }
+}
